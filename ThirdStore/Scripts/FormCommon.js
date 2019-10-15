c363703b5f11136d5dea53b71ca3796003e6f35a
Jim <gdutjim@gmail.com>
2019-10-15 13:31:38 +1100

    disable save button once click

--- ThirdStore/Scripts/FormCommon.js
+++ ThirdStore/Scripts/FormCommon.js
@@ -6,9 +6,27 @@
         }
     });
 
-    $('form').submit(function () {
-        $(this).find("input[type='submit']").each(function () {
-            $(this).attr('disabled', 'disabled');
-        });
+    //$('form').submit(function () {
+    //    $(this).find("input[type='submit']").each(function () {
+    //        //$(this).attr('disabled', 'disabled');
+    //        $(this).prop('disabled', true); 
+    //    });
+    //});
+
+    $("input[type='submit']").each(function () {
+        $(this).click(function (e) {
+            if (e.target) {
+                var attr = $(this).attr('submitting');
+                if (typeof attr !== 'undefined' && attr !== false) { // If button has submitting attribute then do not submit it again.
+                    //$(this).prop('disabled', true);
+                    //$(this).removeAttr("submitting");
+                    e.preventDefault();
+                }
+                else {
+                    $(this).attr("submitting", "true"); // Add "submitting" attribute to prevent multiple submissions.
+                    //e.preventDefault();
+                }
+            }
+        })
     });
 });
