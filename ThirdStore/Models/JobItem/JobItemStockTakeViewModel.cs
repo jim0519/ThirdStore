3f29bbbbfd3558154f5cf35b31c5fd16825e2d76
Jim <gdutjim@gmail.com>
2019-12-20 14:02:20 +1100

    StockTake function

---
+++ ThirdStore/Models/JobItem/JobItemStockTakeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ThirdStoreFramework.MVC;
+
+namespace ThirdStore.Models.JobItem
+{
+    public class JobItemStockTakeViewModel : BaseViewModel
+    {
+        public string JobItemLineID { get; set; }
+        public string JobItemLineReference { get; set; }
+    }
+}
